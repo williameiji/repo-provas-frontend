0f6b88b5b06cf160a6c5a3386daae68cf2ed7132
williameiji <william.fujimori@gmail.com>
2022-09-14 11:20:04 -0300

    fix: change fonts

--- src/assets/css/GlobalStyle.js
+++ src/assets/css/GlobalStyle.js
@@ -7,37 +7,9 @@ const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        font-family: 'Recursive', sans-serif;
+        font-family: 'Poppins', sans-serif;
         background: white;
         margin: 0;
-    }
-
-    @keyframes spin {
-        to {
-            transform: rotate(360deg);
-        }
-    }
-
-    .Modal {
-        position: absolute;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        width: 90%;
-        background: white;
-    }
-
-    .Overlay {
-        position: fixed;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: rgba(0, 0, 0, 0.5);
     }
     
 `;
